b1bebfe7839cb5e2128098b9b99ac350f584c3c3
JadneDream <brady0416@gmail.com>
2020-12-23 15:11:06 +0800

    將各controller回歸到各自模組處理

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,1 +1,22 @@
+import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { UsersService } from './users.service';
 
+@Controller('users')
+export class UsersController {
+    constructor(
+        private readonly usersService: UsersService
+    ) {}
+    
+    @UseGuards(AuthGuard('jwt'))
+	@Get('userlist')
+	getUsers(){
+	    return this.usersService.findAll();
+    }
+    
+    @UseGuards(AuthGuard('jwt'))
+	@Get('profile')
+	getProfile(@Request() req){
+	    return req.user;
+	}
+}
