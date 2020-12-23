bdd4eddf3bb05dc114b9d3be9b3ce2caf67ae991
JadneDream <brady0416@gmail.com>
2020-12-23 11:03:50 +0800

    調整改使用資料庫驗證方式

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,65 +3,18 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entitys/user.entity';
 
-export type User2 = any;
-
 @Injectable()
 export class UsersService {
-  private readonly users: User2[];
-
   constructor(
 	@InjectRepository(User)
 	private usersRepository: Repository<User>
-	
-  ) {
-    this.users = [
-      {
-        userId: 1,
-        username: 'john',
-        password: 'changeme',
-      },
-      {
-        userId: 2,
-        username: 'chris',
-        password: 'secret',
-      },
-      {
-        userId: 3,
-        username: 'maria',
-        password: 'guess',
-      },
-    ];
-  }
-
-  async findOne(username: string): Promise<User2 | undefined> {
-    return this.users.find(user => user.username === username);
-  }
+  ) {}
   
-  	async findAll(): Promise<User[]> {
-  	    return this.usersRepository.find();
-  	}
+  async findAll(): Promise<User[]> {
+      return this.usersRepository.find();
+  }
 	
-	async findOne2(findusername: string): Promise<User | undefined> {
+	async findOne(findusername: string): Promise<User | undefined> {
 		return this.usersRepository.findOne({ account: findusername});
 	}
 }
-
-// @Injectable()
-// export class UsersService {
-// 	constructor(
-// 		@InjectRepository(User)
-// 		private usersRepository: Repository<User>,
-// 	) {}
-	
-// 	async findAll(): Promise<User[]> {
-// 	    return this.usersRepository.find();
-// 	}
-
-// 	async findOne(id: string): Promise<User> {
-// 		return this.usersRepository.findOne(id);
-// 	}
-
-// 	async remove(id: string): Promise<void> {
-// 		await this.usersRepository.delete(id);
-// 	}
-// }
